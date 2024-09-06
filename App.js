cf8e0e3524f8d3b8c5082f0599f4fbd2027fc817
Miklós Fazekas <mfazekas@szemafor.com>
2024-09-06 23:40:24 +0200

    initial version

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+function getSkyColor() {
+  return process.env.SKY_COLOR || 'undefined';
+}
+
+function getGrassColor() {
+  return process.env.GRASS_COLOR || 'undefined';
+}
+
+function getESkyColor() {
+  return process.env.EXPO_PUBLIC_SKY_COLOR || 'undefined';
+}
+
+function getEGrassColor() {
+  return process.env.EXPO_PUBLIC_GRASS_COLOR || 'undefined';
+}
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{}}>The sky is {getSkyColor()}</Text>
+      <Text style={{}}>The grass is {getGrassColor()}</Text>
+
+      <Text style={{}}>The esky is {getESkyColor()}</Text>
+      <Text style={{}}>The egrass is {getEGrassColor()}</Text>
     </View>
   );
 }
