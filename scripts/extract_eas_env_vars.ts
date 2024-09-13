c530a10b925644edc0f032d7196c1bafc1bc5192
Miklós Fazekas <mfazekas@szemafor.com>
2024-09-13 05:48:49 +0200

    env var update

--- scripts/extract_eas_env_vars.ts
+++ scripts/extract_eas_env_vars.ts
@@ -1,16 +1,28 @@
-// Write a script that given a profile name  as an arugment
-// reads the eas.json file an prints the env key value pairs so it can
-// be used to set environment variables in the shell or by adding to $GITHUB_ENV file
+// Extract env variables from eas.json for a specific profile
+// Usage: ts-node scripts/extract_eas_env_vars.ts <profile> $GITHUB_ENV
+// For more info see:
+//   - https://docs.expo.dev/guides/environment-variables/#eas-build
+//   - https://docs.expo.dev/guides/environment-variables/#eas-update
+//   - https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#environment-files
 
 import fs from 'fs';
 import path from 'path';
 
+if (process.argv.length < 4) {
+  console.error('Usage: ts-node scripts/extract_eas_env_vars.ts <profile> $GITHUB_ENV');
+  process.exit(1);
+}
+
 const profile = process.argv[2];
+const envfile = process.argv[3];
 const easJsonPath = path.join(process.cwd(), 'eas.json');
 const easJson = JSON.parse(fs.readFileSync(easJsonPath, 'utf8'));
 
 const env = easJson.build[profile].env;
+const variables: string[] = []
 for (const key in env) {
-  console.log
-  (`${key}=${env[key]}`);
+  variables.push(key);
+  fs.appendFileSync(envfile, `${key}=${env[key]}\n`);
 }
+
+console.log('Variables ${variables.join(', ')} extracted from eas.json for profile ${profile} into ${envfile}');
